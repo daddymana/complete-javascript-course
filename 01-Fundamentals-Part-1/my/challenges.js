fa7a45922867b57a6c4114d9cca3a7988de97353
Daniele Manassero <daniele.manassero@domino.it>
2022-11-16 16:48:56 +0100

    01-Fundamentals-Part-1 done challenge 3 and 4

--- 01-Fundamentals-Part-1/my/challenges.js
+++ 01-Fundamentals-Part-1/my/challenges.js
@@ -1,3 +1,5 @@
+'use strict';
+
 console.log('-> Challenge 1 START 👉');
 
 const markWeight = 78;
@@ -12,6 +14,7 @@ const johnBMI = johnWeight / johnHeight ** 2;
 console.log(markBMI, johnBMI);
 
 console.log('-> Challenge 1 END 👋');
+console.log('******************************');
 
 /******************************************/
 
@@ -22,3 +25,49 @@ if (markBMI > johnBMI) {
   console.log(`John's (${johnBMI}) BMI is higher than Mark's (${markBMI})!`);
 }
 console.log('-> Challenge 2 END 👋');
+console.log('******************************');
+
+/******************************************/
+
+console.log('-> Challenge 3 START 👉');
+
+let mediaDolphins = (96 + 108 + 89) / 3;
+let mediaKoalas = (88 + 91 + 110) / 3;
+
+if (mediaDolphins === mediaKoalas) {
+  console.log('Dolphins and Koalas wins the competition both! 🏆');
+} else if (mediaDolphins > mediaKoalas) {
+  console.log('Dolphins win the competition! 🏆');
+} else {
+  console.log('Koalas win the competition! 🏆');
+}
+
+mediaDolphins = (97 + 112 + 101) / 3;
+mediaKoalas = (109 + 95 + 123) / 3;
+
+if (mediaDolphins !== mediaKoalas && mediaDolphins > mediaKoalas && mediaDolphins >= 100) {
+  console.log('Dolphins win the Bonus 1! 🏆');
+} else if (mediaKoalas > mediaDolphins && mediaKoalas >= 100) {
+  console.log('Koalas win the Bonus 1! 🏆');
+}
+
+mediaKoalas = (109 + 95 + 106) / 3;
+
+if (mediaDolphins === mediaKoalas && mediaDolphins >= 100 && mediaKoalas >= 100) {
+  console.log('Dolphins and Koalas wins the Bonus 2! 🏆');
+}
+
+console.log('-> Challenge 3 END 👋');
+console.log('******************************');
+
+/******************************************/
+
+console.log('-> Challenge 4 START 👉');
+
+const bill = 275;
+let tip = (bill >= 50 && bill <= 300) ? bill * 0.15 : bill * 0.2;
+
+console.log(`The bill was ${bill}€, the tip was ${tip}€, and the total value ${bill + tip}€`);
+
+console.log('-> Challenge 4 END 👋');
+console.log('******************************');
